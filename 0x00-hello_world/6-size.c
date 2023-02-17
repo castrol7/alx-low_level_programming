3c03e202e7ebbf562fd1ae1ce1758f14cfb573a7
castrol7 <oluochcastrol@gmail.com>
2023-02-17 00:12:45 -0800

    good git

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various types on the computer it is compiled and run on
- * Return 0(Success)
+ * Return: 0
  */
 int main(void)
 {
@@ -15,5 +15,5 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	return (0)
+	return (0);
 }
