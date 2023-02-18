d09de9aaea3457456a4c9935a47f530036b6def6
castrol7 <oluochcastrol@gmail.com>
2023-02-18 06:32:16 -0800

    good commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(voi)
+{
+	char letter;
+
+	fochar letter = 'a';
+
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+    return 0;
+}	       
