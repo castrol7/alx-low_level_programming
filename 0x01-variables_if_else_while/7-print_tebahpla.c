c924e5d29a3b967d5733a7a45d663bd170970992
castrol7 <oluochcastrol@gmail.com>
2023-02-18 12:17:04 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,23 +3,16 @@
 /**
 * main - Entry point
 *
-* Description: Prints all single digit numbers of base 10 starting from 0,
-* followed by a new line. Uses only putchar function, twice.
-*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	int num = 48;
+char c;
 
-	while (num < 58)
-	{
-		putchar(num);
-		num++;
-	}
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
 
-	putchar('\n');
-
-	return (0);
+return (0);
 }
 
