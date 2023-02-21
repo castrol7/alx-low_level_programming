46ae605daf21d57c4d777229caefc038d62e1b47
castrol7 <oluochcastrol@gmail.com>
2023-02-21 12:05:24 -0800

    best commiter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+
+putchar('\n');
+}
+
+int main(void)
+{
+print_alphabet();
+return 0;
+}
+
