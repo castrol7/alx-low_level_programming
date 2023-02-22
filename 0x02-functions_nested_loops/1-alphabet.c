b4800aea5d4b7e3def4bf11285cc53f45ba1729c
castrol7 <oluochcastrol@gmail.com>
2023-02-21 22:22:14 -0800

    good commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-void print_alphabet(void);
 int _putchar(char c);
 /**
 * print_alphabet - prints the alphabet in lowercase, followed by a new line
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,8 +1,8 @@
-#ifndef ALPHABET_H
-#define ALPHABET_H
 #include <stdio.h>
 #include "main.h"
+
 void print_alphabet(void);
+
 /**
 * main - check the code
 *
