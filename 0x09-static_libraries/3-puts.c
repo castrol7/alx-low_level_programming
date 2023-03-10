185af75d33def984a5406be4796e94340893e445
castrol7 <oluochcastrol@gmail.com>
2023-03-10 11:06:57 -0800

    good commit

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - Prints a string to stdout.
+* @str: The string to be printed.
+*
+* Description: function takes a string as input and prints it to the
+*              standard output, followed by a newline.
+*/
+void _puts(char *str);
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success).
+*/
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ *
+ * Description: function takes a string as input and prints it to the
+ *              standard output, followed by a newline
+ */
+void _puts(char *str)
+{
+
+}
+
