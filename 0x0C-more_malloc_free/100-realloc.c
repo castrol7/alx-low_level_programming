1b5286d8b9073771f5a88f566e6496b7c42e96a5
castrol7 <oluochcastrol@gmail.com>
2023-03-16 12:12:21 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,37 +3,47 @@
 
 /**
 * _realloc - reallocates a memory block using malloc and free
+* @ptr: pointer to memory previously allocated with a call to malloc
+* @old_size: size, in bytes, of the allocated space for ptr
+* @new_size: new size, in bytes, of the new memory block
 *
-* @ptr: pointer to the memory previously allocated with malloc
-* @old_size: size of the allocated space for ptr
-* @new_size: new size of the memory block
-*
-* Return: On success, a pointer to the newly allocated memory block.
-*         On failure, NULL.
+* Return: pointer to the reallocated memory block
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *new_ptr;
+	void *new_ptr;
 
-if (new_size == 0)
-{
-free(ptr);
-return (NULL);
-}
+	/* If new_size is zero, free ptr and return NULL */
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-if (ptr == NULL)
-return (malloc(new_size));
+	/* If ptr is NULL, allocate new memory and return it */
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
 
-if (new_size <= old_size)
-return (ptr);
+	/* If new_size == old_size, do nothing and return ptr */
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
 
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
-return (NULL);
+	/* Allocate new memory with malloc */
+	new_ptr = malloc(new_size);
 
-memcpy(new_ptr, ptr, old_size);
-free(ptr);
+	/* Copy contents from old memory to new memory */
+	if (new_ptr && ptr)
+	{
+		memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
 
-return (new_ptr);
+		/* Free old memory */
+		free(ptr);
+	}
+
+	return (new_ptr);
 }
 
