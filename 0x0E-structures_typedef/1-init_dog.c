94efef898aef735f62cb4b39fd63c749bdfbf3ac
castrol7 <oluochcastrol@gmail.com>
2023-03-21 09:24:18 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,32 +1,56 @@
-#ifndef INIT_DOG_H
-#define INIT_DOG_H
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "dog.h"
+
+struct dog {
+char name[50];
+float age;
+char owner[50];
+};
 
 /**
-* init_dog - initializes a dog structure with the given values
-* @d: a pointer to a dog structure to initialize
-* @name: a string representing the dog's name
-* @age: a float representing the dog's age
-* @owner: a string representing the dog's owner's name
+* init_dog - initializes a variable of type struct dog
+* @d: pointer to struct dog to be initialized
+* @name: pointer to name of dog
+* @age: age of dog
+* @owner: pointer to name of dog's owner
 *
-* Description: This function initializes a dog structure with the given
-* name, age, and owner's name by dynamically allocating memory for the
-* name and owner strings and setting the dog's age.
+* Return: void
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-/* Allocate memory for the name and owner strings */
-d->name = malloc(strlen(name) + 1);
-d->owner = malloc(strlen(owner) + 1);
-
-/* Copy the name and owner strings into the dog struct */
-strcpy(d->name, name);
-strcpy(d->owner, owner);
-
-/* Set the dog's age */
-d->age = age;
+// Check if the input pointer to struct is valid
+if (d == NULL)
+{
+printf("Error: invalid pointer to struct.\n");
+return;
 }
-#endif /* INIT_DOG_H */
+// Check if the input name is valid
+if (name == NULL)
+{
+printf("Error: invalid name.\n");
+return;
+}
+// Check if the input owner is valid
+if (owner == NULL)
+{
+printf("Error: invalid owner.\n");
+return;
+}
+// Initialize the struct fields with the input values
+strncpy(d->name, name, sizeof(d->name) - 1);
+d->name[sizeof(d->name) - 1] = '\0'; // Ensure null-termination of the string
+d->age = age;
+strncpy(d->owner, owner, sizeof(d->owner) - 1);
+d->owner[sizeof(d->owner) - 1] = '\0'; // Ensure null-termination of the string
+}
+
+int main(void)
+{
+struct dog my_dog;
+init_dog(&my_dog, "Fido", 2.5, "John");
+printf("My dog's name is %s, he is %.1f years old, and his owner is %s.\n",
+my_dog.name, my_dog.age, my_dog.owner);
+return (0);
+}
 
