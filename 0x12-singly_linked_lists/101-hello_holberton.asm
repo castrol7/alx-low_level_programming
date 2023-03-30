b5c7c1a43f51d7d16cb1c31d36dc1a54a469fc92
castrol7 <oluochcastrol@gmail.com>
2023-03-30 10:04:33 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,44 @@
 section .data
-message db 'Hello, Holberton!', 0
-format db '%s\n', 0
+hello db "Hello, Holberton", 10, 0
+hello_len equ $ - hello
+format db "%s", 10, 0
 
 section .text
 global main
-extern printf
+extern printf, malloc, free, exit
 
 main:
-push message
-push format
+; Allocate memory for a list_t node
+mov rdi, 8
+call malloc
+mov qword [rax], 0 ; set next field to NULL
+
+; Allocate memory for the string
+mov rdi, hello_len
+call malloc
+
+; Copy the string into the allocated memory
+mov rsi, hello
+mov rdi, [rax + 8] ; get pointer to str field
+mov ecx, hello_len
+rep movsb
+
+; Set the len field of the node
+mov dword [rax + 16], ecx
+
+; Call printf to print the string
+mov rdi, format
+mov rsi, [rax + 8] ; get pointer to str field
+xor eax, eax
 call printf
-add rsp, 16
-mov eax, 0
-ret
+
+; Free the memory
+mov rdi, [rax + 8] ; get pointer to str field
+call free
+mov rdi, rax ; pointer to node
+call free
+
+; Exit
+xor edi, edi
+call exit
 
