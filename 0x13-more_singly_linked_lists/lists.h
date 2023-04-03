27abaad2e5ffd7a1faa20461514f5b2625cc470e
castrol7 <oluochcastrol@gmail.com>
2023-04-03 09:00:05 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,57 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void free_list2(list_t **head);
+int pop_list(list_t **head);
+list_t *get_node_at_index(list_t *head, unsigned int index);
+list_t *insert_node_at_index(list_t **head, unsigned int idx, const char *str);
+int delete_node_at_index(list_t **head, unsigned int index);
+list_t *reverse_list(list_t **head);
+size_t print_list_safe(const list_t *head);
+size_t free_list_safe(list_t **h);
+list_t *find_list_loop(list_t *head);
+
+#endif /* LISTS_H */
+
