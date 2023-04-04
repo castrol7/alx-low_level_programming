091f6ca72cb3475c8314a8306e7a14f26443fc44
castrol7 <oluochcastrol@gmail.com>
2023-04-03 22:49:01 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -40,6 +40,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int sum_listint(listint_t *head);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
