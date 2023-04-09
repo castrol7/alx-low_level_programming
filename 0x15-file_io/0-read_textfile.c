ac62775a7348d54b527fe3e1b7a2305a12f7724e
castrol7 <oluochcastrol@gmail.com>
2023-04-09 06:22:19 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,45 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
-
-#define BUF_SIZE 1024
-
+#include <unistd.h>
+#include <fcntl.h>
 /**
-* read_textfile - Reads a text file and prints it to the standard output.
-*
-* @filename: The name of the file to read.
-* @letters: The maximum number of letters to read.
-*
-* Return: The actual number of letters read, or 0 if an error occurred.
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: the name of the file to read
+* @letters: the number of letters to read and print
+* Return: the actual number of letters read and printed, or 0 on error
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	int fd;
-	ssize_t n_read, total_read = 0;
+int fd;
+ssize_t bytes_read, bytes_written;
+char *buffer;
 
-	buf = malloc(BUF_SIZE);
-	if (buf == NULL)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buf);
-		return (0);
-	}
-
-	while ((n_read = read(fd, buf, BUF_SIZE)) > 0)
-	{
-		if (write(STDOUT_FILENO, buf, n_read) != n_read)
-		{
-			free(buf);
-			close(fd);
-			return (0);
-		}
-		total_read += n_read;
-		if ((size_t)total_read >= letters)
-			break;
-	}
-
-	free(buf);
-	close(fd);
-	return (total_read);
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buffer = malloc(sizeof(*buffer) * letters);
+if (buffer == NULL)
+{
+close(fd);
+return (0);
 }
-
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+close(fd);
+free(buffer);
+return (0);
+}
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+close(fd);
+free(buffer);
+return (0);
+}
+close(fd);
+free(buffer);
+return (bytes_written);
+}
