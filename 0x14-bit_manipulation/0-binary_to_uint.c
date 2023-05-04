7ca934eef31954144c9147a62a341baec5ae57d8
castrol7 <oluochcastrol@gmail.com>
2023-05-04 09:59:04 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,29 @@
+#include <stddef.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
-* clear_bit - Sets the value of a bit to 0 at a given index.
-* @n: A pointer to an unsigned long integer.
-* @index: The index of the bit to set, starting from 0.
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: pointer to a string of 0 and 1 chars
 *
-* Return: 1 if it worked, or -1 if an error occurred.
+* Return: the converted number, or 0 if there is one or more chars in the
+*         string b that is not 0 or 1, or if b is NULL
 */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int binary_to_uint(const char *b)
 {
-/* Check if index is within the range of bits of unsigned long int */
-if (index >= sizeof(unsigned long int) * 8)
+unsigned int num = 0;
+
+if (b == NULL)
+return 0;
+
+while (*b != '\0')
 {
-return (-1);
+if (*b != '0' && *b != '1')
+return 0;
+
+num = (num << 1) + (*b - '0');
+b++;
 }
 
-/* Create a mask with a 0 bit at the given index */
-unsigned long int mask = ~(1UL << index);
-
-/* Clear the bit at the given index in the number */
-*n &= mask;
-
-return (1);
+return num;
 }
 
-int main(void)
-{
-unsigned long int n = 109;       /* 0b1101101 in binary */
-int result = clear_bit(&n, 3);   /* Clear bit at index 3 */
-
-if (result == 1)
-{
-printf("The new value of n is: %lu\n", n);
-}
-else
-{
-printf("An error occurred.\n");
-}
-
-return (0);
-}
